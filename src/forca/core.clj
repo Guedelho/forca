9e1d29a49a7743445e61e07c9a3183add7402062
Guedelho <mateusguedelho@yahoo.com.br>
2019-08-05 14:42:57 -0300

    functions and fib

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,6 +3,19 @@
 
 (def total-de-vidas 6)
 
+(defn perdeu [] (print "Você perdeu"))
+
+(defn jogo [vidas]
+  (if (= vidas 0)
+    (perdeu)
+    (do
+      (print vidas)
+      (jogo (dec vidas)))))
+
+(defn fib [n]
+  (if (or (= n 0) (= n 1)) n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
