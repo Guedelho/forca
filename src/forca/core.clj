0304712f8cc48c34433a4b9435917a6b08a78c51
Guedelho <mateusguedelho@yahoo.com.br>
2019-08-12 21:31:52 -0300

    read-line

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,19 +3,33 @@
 
 (def total-de-vidas 6)
 
+(defn le-letra! [] (read-line))
+
 (defn perdeu [] (print "Você perdeu"))
+
 (defn ganhou [] (print "Você ganhou"))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
 (defn letras-faltantes [palavra acertos]
   (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos)))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)))
 
 (defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
     (if (acertou-a-palavra-toda? palavra acertos)
       (ganhou)
-      (print "Chuta, amigo!"))))
+      (avalia-chute (le-letra!) vidas palavra acertos))))
+
+
 
 (defn fib [n]
   (if (or (= n 0) (= n 1)) n
@@ -25,3 +39,5 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+; (require '[forca.core :as forca] :reload)
