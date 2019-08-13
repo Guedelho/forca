eeb9ec893d4593c61ed389a63ad9240ad57318d9
Guedelho <mateusguedelho@yahoo.com.br>
2019-08-13 00:57:53 -0300

    recur and loops

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -17,23 +17,34 @@
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos)))
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)))
-
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos))))
-
-
+  (cond
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+    :else
+    (let [chute (le-letra!)]
+      (if (acertou? chute palavra)
+        (do
+          (println "Acertou a letra!")
+          (recur vidas palavra (conj acertos chute)))
+        (do
+          (println "Errou a letra! Perdeu vida!")
+          (recur (dec vidas) palavra acertos))))))
 
 (defn fib [n]
   (if (or (= n 0) (= n 1)) n
       (+ (fib (- n 1)) (fib (- n 2)))))
+
+(defn fib2 [n]
+  (loop [a 1 b 1 numero 2]
+    (if
+     (= numero n) b
+     (recur b (+ a b) (inc numero)))))
+
+(defn soma [n]
+  (loop [contador 1 soma 0]
+    (if (> contador n) soma
+        (recur (inc contador) (+ contador soma)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
